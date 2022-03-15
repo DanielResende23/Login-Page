e5bee3a99d995296d22886be919abb17a947b1eb
Daniel Resende <97549888+DanielResende23@users.noreply.github.com>
2022-03-14 21:52:18 -0300

    Update olho.js

--- olho.js
+++ olho.js
@@ -1,12 +1,17 @@
-document.getElementById('olho').addEventListener('mousedown', function() {
+function mostrarSenha() {
     document.getElementById('pass').type = 'text';
-});
+}
 
-document.getElementById('olho').addEventListener('mouseup', function() {
+function ocultarSenha() {
     document.getElementById('pass').type = 'password';
-});
+}
+
+function seguranca() {
+    document.getElementById('pass').type = 'password';
+}
+
+document.getElementById('olho').addEventListener('mousedown', mostrarSenha)
+document.getElementById('olho').addEventListener('mouseup', ocultarSenha)
 
 // Para que o password não fique exposto apos mover a imagem.
-document.getElementById('olho').addEventListener('mousemove', function() {
-    document.getElementById('pass').type = 'password';
-});
+document.getElementById('olho').addEventListener('mousemove', seguranca)
